972d1287837e60588d599cf25095711d4f352daf
Madhan235 <msouljar@gmail.com>
2023-06-20 09:15:15 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
  import express from 'express';
 import {  studentsRouter } from './routers/students.js';
 import { mentorsRouter } from './routers/mentor.js';
- 
+ import dotenv from "dotenv";
+//config dotenv
+ dotenv.config();
 //initializing server
  const app = express();
  //middleware for post and put
@@ -14,5 +16,5 @@ app.use("/students",studentsRouter)
 //mentorsRouter
 app.use("/mentors", mentorsRouter)
 //starting the app
- app.listen(port,()=>console.log(`localhost:${port}`));
+ app.listen(`${port}`,()=>console.log(`localhost:${port}`));
  
