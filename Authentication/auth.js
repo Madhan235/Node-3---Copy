cdc516237dc6bedcd4040e0c07d9e244253f0ab9
Madhan235 <msouljar@gmail.com>
2023-06-22 14:58:14 +0530

    yes

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -1,17 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export async function isAuthenticated(req,res,next){
-   try {
+   
     const token = req.headers["auth-token"];
  if(!token){
-   return res.status(400).json({data: "Token not found"})
- }
-    const validateUser = 
+   return res.status(400).json({data: "Invalid authorization"})
+ } 
     jwt.verify(token, process.env.secretkey)
 next()
-   } catch (error) {
-    console.log(error)
-    res.status(400).json({data: "Invalid authorization"})
-   }
+   
  
 }
